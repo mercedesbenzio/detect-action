e42e8e1d86b35ccfaf6fcdb1b5b9122c687e0529
Tiago Santos <tvcsantos@gmail.com>
2023-08-28 08:37:29 +0100

    feat!: revamp action

--- src/detect/exit-code.ts
+++ src/detect/exit-code.ts
@@ -1,7 +1,5 @@
-import { info, setOutput } from '@actions/core'
-import { TOOL_NAME } from './detect-manager'
-
 export enum ExitCode {
+  // noinspection SpellCheckingInspection
   SUCCESS = 0, //Detect exited successfully.
   FAILURE_BLACKDUCK_CONNECTIVITY = 1, //Detect was unable to connect to Black Duck. Check your configuration and connection.
   FAILURE_TIMEOUT = 2, //Detect was unable to wait for actions to be completed on Black Duck. Check your Black Duck server or increase your timeout.
@@ -9,9 +7,9 @@ export enum ExitCode {
   FAILURE_PROXY_CONNECTIVITY = 4, //Detect was unable to use the configured proxy. Check your configuration and connection.
   FAILURE_DETECTOR = 5, //Detect had one or more detector failures while extracting dependencies. Check that all projects build and your environment is configured correctly.
   FAILURE_SCAN = 6, //Detect was unable to run the signature scanner against your source. Check your configuration.
-  FAILURE_CONFIGURATION = 7, //Detect was unable to start due to issues with it's configuration. Check and fix your configuration.
+  FAILURE_CONFIGURATION = 7, //Detect was unable to start due to issues with its configuration. Check and fix your configuration.
   FAILURE_DETECTOR_REQUIRED = 9, //Detect did not run all of the required detectors. Fix detector issues or disable required detectors.
-  FAILURE_BLACKDUCK_VERSION_NOT_SUPPORTED = 10, //Detect's configuration requires a Black Duck capability that is not supported by your version of Black Duck. Ensure that your Black Duck version is compatible with this version of Detect.
+  FAILURE_BLACKDUCK_VERSION_NOT_SUPPORTED = 10, //Detect configuration requires a Black Duck capability that is not supported by your version of Black Duck. Ensure that your Black Duck version is compatible with this version of Detect.
   FAILURE_BLACKDUCK_FEATURE_ERROR = 11, //Detect encountered an error while attempting an operation on Black Duck. Ensure that your Black Duck version is compatible with this version of Detect, and that your Black Duck user account has the required roles.
   FAILURE_MINIMUM_INTERVAL_NOT_MET = 13, //Detect did not wait the minimum required scan interval.
   FAILURE_IAC = 14, //Detect was unable to perform IaC Scan against your source. Please check your configuration, and see logs and IaC Scanner documentation for more information.
@@ -21,16 +19,6 @@ export enum ExitCode {
   FAILURE_UNKNOWN_ERROR = 100 //Detect encountered an unknown error.
 }
 
-export function getExitCodeName(exitCode: number) {
+export function getExitCodeName(exitCode: number): string {
   return ExitCode[exitCode] || 'UNKNOWN'
-}
-
-export function setExitCodeOutputsIfDefined(exitCode: number | void) {
-  let exitCodeName = undefined
-  if (exitCode !== undefined) {
-    exitCodeName = getExitCodeName(exitCode)
-    setOutput('detect-exit-code', exitCode)
-    setOutput('detect-exit-code-name', exitCodeName)
-  }
-  info(`${TOOL_NAME} exited with code ${exitCode} - ${exitCodeName}`)
 }
